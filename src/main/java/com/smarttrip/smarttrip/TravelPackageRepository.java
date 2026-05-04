acaceadc6792c9996a4a29b00ea3bb65841f2959
nh <macbook@MacBooks-MacBook-Air.local>
2026-05-04 15:54:13 +0600

    Add travel package model and repository

--- src/main/java/com/smarttrip/smarttrip/TravelPackageRepository.java
+++ src/main/java/com/smarttrip/smarttrip/TravelPackageRepository.java
@@ -1,6 +1,8 @@
 package com.smarttrip.smarttrip;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
 public interface TravelPackageRepository extends JpaRepository<TravelPackage, Long> {
+    List<TravelPackage> findByStatus(String status);
 }
